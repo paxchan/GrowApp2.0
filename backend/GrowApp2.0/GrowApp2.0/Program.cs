3be3cbf2b0574eadc49fdfe94a1f6c3ce97c3d3c
Kate Sargent <katesargentt@gmail.com>
2025-03-13 13:16:13 -0600

    added connection string (in comments)

--- backend/GrowApp2.0/GrowApp2.0/Program.cs
+++ backend/GrowApp2.0/GrowApp2.0/Program.cs
@@ -4,6 +4,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+//Kate's part with the connection string & Dbcontext connection
+// builder.Services.AddDbContext<GrowDbContext>(options =>
+//     options.UseSqlite(builder.Configuration.GetConnectionString("GrowConnection")));
+//"ConnectionStrings": {
+//    "GrowConnection": "Data Source=Grow.sqlite"
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
